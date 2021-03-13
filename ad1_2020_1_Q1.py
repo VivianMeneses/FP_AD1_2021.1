ba541071db48d975a3b713ebdd3f73a12141ca63
Vivian Meneses <62348089+VivianMeneses@users.noreply.github.com>
2021-03-12 21:42:04 -0300

    Update ad1_2020_1_Q1.py
    Retirei a duplicada de print da distância percorrida

--- ad1_2020_1_Q1.py
+++ ad1_2020_1_Q1.py
@@ -29,6 +29,5 @@ if m > 3.16:
     n += 1    
 
 print("Distância percorrida: ", km_f ," km")
-print("Distância percorrida: ", km ," km")
 print("Diâmetro da roda: ", d_cm, " cm")
 print("Ordem de grandeza da quantidade de voltas efetuadas: 10 elevado a ", n)
